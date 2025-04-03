21723e572b6d8f67f5a766f096df304e19255684
VManuelPM <v.manuel.nago@gmail.com>
2025-04-03 18:21:56 +0200

    Add email sending functionality with single and multiple attachments

--- src/main/java/com/amoelcodigo/smtpjava/controller/EmailController.java
+++ src/main/java/com/amoelcodigo/smtpjava/controller/EmailController.java
@@ -1,10 +1,14 @@
 package com.amoelcodigo.smtpjava.controller;
 
 import com.amoelcodigo.smtpjava.services.EmailService;
+import jakarta.mail.MessagingException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/email")
@@ -21,5 +25,33 @@ public class EmailController {
                                @RequestParam String body) {
         emailService.sendEmail(to, subject, body);
         return "Correo enviado correctamente a " + to;
+    }
+
+    @PostMapping("/sendWithAttachment")
+    public String sendEmailWithAttachment(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String body,
+            @RequestParam String filePath) {
+        try {
+            emailService.sendEmailWithAttachment(to, subject, body, filePath);
+            return "Correo con adjunto enviado a " + to;
+        } catch (MessagingException e) {
+            return "Error al enviar el correo: " + e.getMessage();
+        }
+    }
+
+    @PostMapping("/sendWithMultipleAttachment")
+    public String sendEmailWithMultipleAttachment(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String body,
+            @RequestParam List<MultipartFile> files) {
+        try {
+            emailService.sendEmailWithMultipleAttachment(to, subject, body, files);
+            return "Correo con adjunto enviado a " + to;
+        } catch (MessagingException e) {
+            return "Error al enviar el correo: " + e.getMessage();
+        }
     }
 }
--- src/main/java/com/amoelcodigo/smtpjava/services/EmailService.java
+++ src/main/java/com/amoelcodigo/smtpjava/services/EmailService.java
@@ -1,8 +1,16 @@
 package com.amoelcodigo.smtpjava.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
 
 @Service
 public class EmailService {
@@ -17,7 +25,38 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        message.setFrom("v.manuel.nago@gmail.com");
+        message.setFrom("amoelcodigotest@gmail.com");
         mailSender.send(message);
+    }
+
+    public void sendEmailWithAttachment(String to, String subject, String text, String filePath) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+        helper.setFrom("amoelcodigotest@gmail.com");
+
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        helper.addAttachment(file.getFilename(), file);
+
+        mailSender.send(mimeMessage);
+    }
+
+    public void sendEmailWithMultipleAttachment(String to, String subject, String text, List<MultipartFile> files) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+        helper.setFrom("amoelcodigotest@gmail.com");
+
+        for (MultipartFile file: files){
+            helper.addAttachment(file.getOriginalFilename(), file);
+        }
+
+        mailSender.send(mimeMessage);
     }
 }
